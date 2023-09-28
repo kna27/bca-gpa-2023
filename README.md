61608fa0e37c0f654872dd8bb5bc0df256b1aad0
Tim Rolshud <68395794+timrolsh@users.noreply.github.com>
2023-09-27 20:15:26 -0400

    fix "P" grade causing GPA to not show, add Safari guide (#2)

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
 # bca-gpa-2023
+
 An update version of [the BCA GPA browser extension](https://github.com/alebod21/bcagpa) that fixes bugs due to PowerSchool updates, and also adds a port to Firefox.
 
 Chrome Web Store Page: https://chrome.google.com/webstore/detail/bca-gpa/ofhfmclagjfikdfhjnogmcokilbikmpf
 
 Firefox Addon Page: https://addons.mozilla.org/en-US/firefox/addon/bca-gpa/
 
-![Screenshot of extension](./screenshot.png)
+![Screenshot of extension](https://github.com/kna27/bca-gpa-2023/assets/68395794/6eac9b8a-a034-4583-8a0d-e7b72efcb508)
+
+# Using BCA GPA on Safari
+
+BCA GPA is not available as an extension for Safari yet, but here is a method of achieving the same functionality as on Chrome or Firefox:
+
+-   Install the [Userscripts](https://apps.apple.com/ua/app/userscripts/id1463298887) extension for Safari through the Mac App Store
+    -   We will use this to run BCA GPA on Powerschool
+    -   This extension works exactly like Greasyfork or Tampermonkey and lets you run your own scripts on websites.
+-   Add the Userscripts extension to the toolbar, click on the icon and go to "Open Extension Page"
+    ![](https://github.com/timrolsh/bca-gpa-2023/assets/68395794/ee236710-ea38-4b87-9cc3-cbbf5af688c2)
+-   You should then be brought to a page like this:
+    ![](https://github.com/timrolsh/bca-gpa-2023/assets/68395794/40dec63e-aa66-4ab7-ba43-0705d390e034)
+-   Use the menu to add a new Javascript script
+    ![](https://github.com/timrolsh/bca-gpa-2023/assets/68395794/e6baf820-f7a3-4d1f-98cb-708f62e1a769)
+-   Copy and paste everything from [this link](https://raw.githubusercontent.com/kna27/bca-gpa-2023/main/SAFARI/BCAGPA.js) and replace everything in the textbox with what you copied
+-   it should look like this:
+    ![](<[../images/4.png](https://github.com/timrolsh/bca-gpa-2023/assets/68395794/f142c4dd-8de0-4aa3-a0e8-b0733c4f5801)>)
+-   Make sure to hit "Save" at the bottom right of the screen to apply the changes in the userscript settings.
+-   Reload or visit the Powerschool page and you should see your GPA there. It should look like this:
+    ![](https://github.com/kna27/bca-gpa-2023/assets/68395794/6eac9b8a-a034-4583-8a0d-e7b72efcb508)
+-   If you don't see it, make sure that the Userscript is activated in the menu, it should look like this:
+    ![](https://github.com/timrolsh/bca-gpa-2023/assets/68395794/c00b5f10-24a0-4e52-9c4b-2800e568973a)
--- src/main.js
+++ src/main.js
@@ -1,151 +1,164 @@
-function extractLetterGrade(str) {
-    var s = "";
-    for (var i = 0; i < str.length; i++) {
-        if (!(str[i] >= '0' && str[i] <= '9')) s += str[i];
-        else return s;
+// ==UserScript==
+// @name        BCA GPA
+// @description GPA Calculator for BCA
+// @match       https://ps001.bergen.org/*
+// ==/UserScript==
+
+if (document.URL === "https://ps001.bergen.org/guardian/home.html") {
+    /**
+     *
+     * @param {String} str Content of grade table cell
+     * @returns {String} Letter grade
+     * @example extractLetterGrade("A95") // "A"
+     */
+    function extractLetterGrade(str) {
+        var s = "";
+        for (var i = 0; i < str.length; i++) {
+            if (!(str[i] >= "0" && str[i] <= "9")) s += str[i];
+            else return s;
+        }
+        if (s == "--" || s == "[ i ]") return "N";
+        return s;
     }
-    if (s == "--" || s == "[ i ]") return "N";
-    return s;
-}
 
-function modsPerWeek(str) {
-    if (str.substring(0, 5) == "25-27" || str == "04-09(W)") return 2;
-    var mods = 0;
+    /**
+     *
+     * @param {String} str Content of exp table cell
+     * @returns {Number} Number of mods per week
+     * @todo Rewrite to use periods instead of mods
+     * @example modsPerWeek("2(M-T,R-F)") // 4
+     */
+    function modsPerWeek(str) {
+        console.log(str);
+        if (str.substring(0, 5) == "25-27" || str == "04-09(W)") return 2;
+        var mods = 0;
 
-    var d = {};
-    d['M'] = 0; d['T'] = 1; d['W'] = 2; d['R'] = 3; d['F'] = 4;
+        var d = {};
+        d["M"] = 0;
+        d["T"] = 1;
+        d["W"] = 2;
+        d["R"] = 3;
+        d["F"] = 4;
 
-    var mod = new Array(5);
-    for (var i = 0; i < 5; i++) {
-        mod[i] = new Array(3);
-        for (var j = 0; j < 3; j++) {
-            mod[i][j] = 0;
+        var mod = new Array(5);
+        for (var i = 0; i < 5; i++) {
+            mod[i] = new Array(3);
+            for (var j = 0; j < 3; j++) {
+                mod[i][j] = 0;
+            }
         }
-    }
-    var t = str.split(" ");
+        var t = str.split(" ");
 
-    var e = Math.min(str.indexOf("-"), str.indexOf('('));
-    var startMod = parseInt(str.substring(0, e));
+        var e = Math.min(str.indexOf("-"), str.indexOf("("));
+        var startMod = parseInt(str.substring(0, e));
 
-    for (var i = 0; i < t.length; i++) {
-        var b2 = t[i].indexOf("(");
-        var b1 = t[i].substring(0, b2).indexOf("-");
-        var b3 = t[i].indexOf(")");
+        for (var i = 0; i < t.length; i++) {
+            var b2 = t[i].indexOf("(");
+            var b1 = t[i].substring(0, b2).indexOf("-");
+            var b3 = t[i].indexOf(")");
 
-        var s, e;
-        if (b1 == -1) {
-            s = parseInt(t[i].substring(0, b2));
-            e = s;
-        }
-        else {
-            s = parseInt(t[i].substring(0, b1));
-            e = parseInt(t[i].substring(b1 + 1, b2));
-        }
+            var s, e;
+            if (b1 == -1) {
+                s = parseInt(t[i].substring(0, b2));
+                e = s;
+            } else {
+                s = parseInt(t[i].substring(0, b1));
+                e = parseInt(t[i].substring(b1 + 1, b2));
+            }
 
-        var parts = t[i].substring(b2 + 1, b3).split(",");
+            var parts = t[i].substring(b2 + 1, b3).split(",");
 
-        for (var j = 0; j < parts.length; j++) {
-            var bb = parts[j].indexOf("-");
-            var sd = d[parts[j][0]], ed;
-            if (bb == -1) ed = sd;
-            else ed = d[parts[j][2]];
+            for (var j = 0; j < parts.length; j++) {
+                var bb = parts[j].indexOf("-");
+                var sd = d[parts[j][0]],
+                    ed;
+                if (bb == -1) ed = sd;
+                else ed = d[parts[j][2]];
 
-            for (var day = sd; day <= ed; day++) {
-                for (var k = s; k <= e; k++) {
-
-                    mod[day][k - startMod] = 1;
+                for (var day = sd; day <= ed; day++) {
+                    for (var k = s; k <= e; k++) {
+                        mod[day][k - startMod] = 1;
+                    }
                 }
             }
         }
-    }
-    for (var i = 0; i < 5; i++) {
-        for (var j = 0; j < 3; j++) {
-            mods += mod[i][j];
-        }
-    }
-    return mods;
-}
-
-function classData() {
-    src = document.documentElement.outerHTML;
-    var classes = document.querySelectorAll('tr[id^="ccid"]');
-
-    var data = []
-
-    for (var i = 0; i < classes.length; i++) {
-        var name = classes.item(i).querySelector('td[class="table-element-text-align-start"]');
-        name = name.textContent;
-        if (name[0] == '~') continue; //Not included in GPA
-
-        var info = classes.item(i).querySelectorAll('td');
-        var grades = ["N", "N", "N", "N", 0];
-
-        var hours = info.item(0).textContent;
-        grades[4] = modsPerWeek(hours);
-
-
-        for (var j = 0; j < info.length; j++) {
-            var elt = info.item(j);
-            var elta = elt.querySelector('a');
-            if (elta == null) continue;
-            var href = elta.href;
-            if (href.substring(href.length - 14, href.length - 12) == "T1") {
-                grades[0] = extractLetterGrade(elta.textContent);
-            }
-            if (href.substring(href.length - 14, href.length - 12) == "T2") {
-                grades[1] = extractLetterGrade(elta.textContent);
-            }
-            if (href.substring(href.length - 14, href.length - 12) == "T3") {
-                grades[2] = extractLetterGrade(elta.textContent);
-            }
-            if (href.substring(href.length - 14, href.length - 12) == "Y1") {
-                grades[3] = extractLetterGrade(elta.textContent);
+        for (var i = 0; i < 5; i++) {
+            for (var j = 0; j < 3; j++) {
+                mods += mod[i][j];
             }
         }
-        data.push(grades);
+        return mods;
     }
-    return data;
-}
 
-var data = classData();
-var pts = {};
-pts["A"] = 4.000;
-pts["A-"] = 3.800;
-pts["B+"] = 3.300;
-pts["B"] = 3.000;
-pts["B-"] = 2.800;
-pts["C+"] = 2.300;
-pts["C"] = 2.000;
-pts["C-"] = 1.800;
-pts["D+"] = 1.300;
-pts["D"] = 1.100;
-pts["F"] = 0.000;
-var GPA = new Array(4);
-var keys = ["Trimester 1", "Trimester 2", "Trimester 3", "Year GPA"];
-for (var i = 0; i < 4; i++) { //Each trimester and final GPA
-    var totPts = 0;
-    var totMods = 0;
-    for (var j = 0; j < data.length; j++) {
-        var entry = data[j];
-        if (entry[i] == "N") continue;
-        totPts += pts[entry[i]] * entry[4] / 2;
-        totMods += entry[4];
+    function classData() {
+        src = document.documentElement.outerHTML;
+        var classes = document.querySelectorAll('tr[id^="ccid"]');
+
+        var data = [];
+
+        for (var i = 0; i < classes.length; i++) {
+            var name = classes.item(i).querySelector('td[class="table-element-text-align-start"]');
+            name = name.textContent;
+            if (name[0] == "~") continue; // Not included in GPA
+
+            var info = classes.item(i).querySelectorAll("td");
+            var grades = ["N", "N", "N", "N", 0];
+
+            var hours = info.item(0).textContent;
+            grades[4] = modsPerWeek(hours);
+
+            for (var j = 0; j < info.length; j++) {
+                var elta = info.item(j).querySelector("a");
+                if (elta == null) continue;
+                let href = elta.href.substring(elta.href.length - 14, elta.href.length - 12);
+                grades[["T1", "T2", "T3", "Y1"].indexOf(href)] = extractLetterGrade(elta.textContent);
+            }
+            data.push(grades);
+        }
+        return data;
     }
-    totMods /= 2;
-    var gpa = totPts / totMods;
-    GPA[i] = (Math.round(gpa * 1000) / 1000).toFixed(3);
-    if (GPA[i] == 'NaN') GPA[i] = 'N/A';
+
+    var data = classData();
+    var pts = {
+        "A": 4.0,
+        "P": 4.0,
+        "A-": 3.8,
+        "B+": 3.3,
+        "B": 3.0,
+        "B-": 2.8,
+        "C+": 2.3,
+        "C": 2.0,
+        "C-": 1.8,
+        "D+": 1.3,
+        "D": 1.1,
+        "F": 0.0
+    };
+    var GPA = new Array(4);
+    var keys = ["Trimester 1", "Trimester 2", "Trimester 3", "Year GPA"];
+    for (var i = 0; i < 4; i++) {
+        // Each trimester and final GPA
+        var totPts = 0;
+        var totMods = 0;
+        for (var j = 0; j < data.length; j++) {
+            var entry = data[j];
+            if (entry[i] == "N") continue;
+            totPts += (pts[entry[i]] * entry[4]) / 2;
+            totMods += entry[4];
+        }
+        totMods /= 2;
+        var gpa = totPts / totMods;
+        GPA[i] = (Math.round(gpa * 1000) / 1000).toFixed(3);
+        if (GPA[i] == "NaN") GPA[i] = "N/A";
+    }
+
+    var gpabox = document.createElement("h2");
+    gpabox.innerHTML = `
+<table cellspacing='0' style='width:50%;'>
+${keys.map((key, i) => `<tr><td>${key}</td><td>${GPA[i]}</td></tr>`).join("")}
+</table>`;
+
+    var contentArea = document.getElementById("content-main");
+    var box = contentArea.querySelector("h1");
+    contentArea.insertBefore(box.cloneNode(true), box);
+    contentArea.replaceChild(gpabox, box);
 }
-
-var gpabox = document.createElement("h2");
-var gpasrc = "<table cellspacing='0' style='width:50%;'>\
-<tr><td>Trimester 1</td><td>" + GPA[0] + "</td></tr>\
-<tr><td>Trimester 2</td><td>" + GPA[1] + "</td></tr>\
-<tr><td>Trimester 3</td><td>" + GPA[2] + "</td></tr>\
-<tr><td>Year GPA</td><td>" + GPA[3] + "</td></tr></table>"
-gpabox.innerHTML = gpasrc;
-
-var contentArea = document.getElementById("content-main");
-var box = contentArea.querySelector("h1");
-contentArea.insertBefore(box.cloneNode(true), box);
-contentArea.replaceChild(gpabox, box);
